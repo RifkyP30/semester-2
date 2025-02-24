b71742cb1fb061872b6fd710b018e41e3f137fd2
Rifky <rifkypraditya@gmail.com>
2025-02-24 21:21:39 +0700

    modifikasi file MatakuliahDemo15.java

--- prak-asd/jobsheet-3/MatakuliahDemo15.java
+++ prak-asd/jobsheet-3/MatakuliahDemo15.java
@@ -3,34 +3,19 @@ import java.util.Scanner;
 public class MatakuliahDemo15 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    Matakuliah15[] arrayOfMatakuliah = new Matakuliah15[3];
-    String kode, nama, dummy;
-    int sks, jumlahJam;
+    System.out.print("Masukkan jumlah mata kuliah: ");
+    int jmlMk = sc.nextInt();
+    Matakuliah15[] arrayOfMatakuliah = new Matakuliah15[jmlMk];
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < arrayOfMatakuliah.length; i++) {
+      arrayOfMatakuliah[i] = new Matakuliah15();
       System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-      System.out.print("Kode      : ");
-      kode = sc.nextLine();
-      System.out.print("Nama      : ");
-      nama = sc.nextLine();
-      System.out.print("Sks       : ");
-      dummy = sc.nextLine();
-      sks = Integer.parseInt(dummy);
-      System.out.print("Jumlah Jam: ");
-      dummy = sc.nextLine();
-      jumlahJam = Integer.parseInt(dummy);
-      System.out.println("------------------------------");
-
-      arrayOfMatakuliah[i] = new Matakuliah15(kode, nama, sks, jumlahJam);
+      arrayOfMatakuliah[i].tambahData();
     }
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < arrayOfMatakuliah.length; i++) {
       System.out.println("Data Matakuliah ke-" + (i + 1));
-      System.out.println("Kode      : " + arrayOfMatakuliah[i].kode);
-      System.out.println("Nama      : " + arrayOfMatakuliah[i].nama);
-      System.out.println("Sks       : " + arrayOfMatakuliah[i].sks);
-      System.out.println("Jumlah Jam: " + arrayOfMatakuliah[i].jumlahJam);
-      System.out.println("------------------------------");
+      arrayOfMatakuliah[i].cetakInfo();
     }
   }
 }
