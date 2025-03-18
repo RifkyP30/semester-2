9c87bfccc2e777e9bcd6e34d57c3a78b9d93f355
Rifky <rifkypraditya@gmail.com>
2025-03-18 11:49:13 +0700

    modifikasi file SortingMain15.java (selection sort)

--- prak-asd/jobsheet-6/SortingMain15.java
+++ prak-asd/jobsheet-6/SortingMain15.java
@@ -2,14 +2,22 @@ public class SortingMain15 {
 
   public static void main(String[] args) {
     int[] a = {20, 10, 2, 7, 12};
+    int[] b = {30, 20, 2, 8, 14};
 
     Sorting15 dataurut1 = new Sorting15(a, a.length);
+    Sorting15 dataurut2 = new Sorting15(b, b.length);
 
     System.out.println("Data awal 1");
     dataurut1.tampil();
     dataurut1.bubbleSort();
     System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
     dataurut1.tampil();
+
+    System.out.println("\nData awal 2");
+    dataurut2.tampil();
+    dataurut2.selectionSort();
+    System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+    dataurut2.tampil();
   }
 
 }
