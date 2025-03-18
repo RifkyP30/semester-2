a03955be669f9f340c2c85b4cfb3aa05f64dd9f4
Rifky <rifkypraditya@gmail.com>
2025-03-18 12:02:56 +0700

    modifikasi file SortingMain15.java (insertion sort)

--- prak-asd/jobsheet-6/SortingMain15.java
+++ prak-asd/jobsheet-6/SortingMain15.java
@@ -3,9 +3,11 @@ public class SortingMain15 {
   public static void main(String[] args) {
     int[] a = {20, 10, 2, 7, 12};
     int[] b = {30, 20, 2, 8, 14};
+    int[] c = {40, 10, 4, 9, 3};
 
     Sorting15 dataurut1 = new Sorting15(a, a.length);
     Sorting15 dataurut2 = new Sorting15(b, b.length);
+    Sorting15 dataurut3 = new Sorting15(c, c.length);
 
     System.out.println("Data awal 1");
     dataurut1.tampil();
@@ -18,6 +20,12 @@ public class SortingMain15 {
     dataurut2.selectionSort();
     System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
     dataurut2.tampil();
+
+    System.out.println("\nData awal 3");
+    dataurut3.tampil();
+    dataurut3.insertionSort();
+    System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+    dataurut3.tampil();
   }
 
 }
