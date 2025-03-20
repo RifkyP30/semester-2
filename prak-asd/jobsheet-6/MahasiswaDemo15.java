a0a306e1af02c844be567b11356d6d42fdf5929e
Rifky <rifkypraditya@gmail.com>
2025-03-20 21:32:11 +0700

    modifikasi file MahasiswaDemo15.java (input dari keyboard)

--- prak-asd/jobsheet-6/MahasiswaDemo15.java
+++ prak-asd/jobsheet-6/MahasiswaDemo15.java
@@ -1,25 +1,37 @@
+import java.util.Scanner;
+
 public class MahasiswaDemo15 {
 
   public static void main(String[] args) {
-    MahasiswaBerprestasi15 list = new MahasiswaBerprestasi15();
-    Mahasiswa15 m1 = new Mahasiswa15("123", "Zidan", "2A", 3.2);
-    Mahasiswa15 m2 = new Mahasiswa15("124", "Ayu", "2A", 3.5);
-    Mahasiswa15 m3 = new Mahasiswa15("125", "Sofi", "2A", 3.1);
-    Mahasiswa15 m4 = new Mahasiswa15("126", "Sita", "2A", 3.9);
-    Mahasiswa15 m5 = new Mahasiswa15("127", "Miki", "2A", 3.7);
+    Scanner input15 = new Scanner(System.in);
+    // input jumlah mahasiswa
+    System.out.print("\nMasukkan jumlah mahasiswa: ");
+    int jmlMhs = input15.nextInt();
+    // membuat array of object
+    Mahasiswa15[] dataMhs = new Mahasiswa15[jmlMhs];
+    // membuat object dari class MahasiswaBerprestasi15
+    MahasiswaBerprestasi15 daftar = new MahasiswaBerprestasi15(jmlMhs);
 
-    list.tambah(m1);
-    list.tambah(m2);
-    list.tambah(m3);
-    list.tambah(m4);
-    list.tambah(m5);
+    // mengisi data mahasiswa
+    for (int i = 0; i < dataMhs.length; i++) {
+      System.out.println("\nMasukkan data mahasiswa ke-" + (i + 1));
+      dataMhs[i] = new Mahasiswa15();
+      dataMhs[i].isiData();
+    }
 
-    System.out.println("Data mahasiswa sebelum sorting: ");
-    list.tampil();
+    input15.close();
+    System.out.println("----------------------------------------");
 
-    System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC): ");
-    list.bubbleSort();
-    list.tampil();
+    daftar.tambah(dataMhs); 
+
+    System.out.println("\nData mahasiswa sebelum sorting: ");
+    daftar.tampil();
+
+    daftar.bubbleSort();
+
+    System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK (DESC): ");
+    daftar.tampil();
+
   }
   
 }
