2d638517d77b892c26a1ce11fdde66f3b665ddd2
Rifky <rifkypraditya@gmail.com>
2025-05-09 22:09:10 +0700

    modifikasi file AntrianLayanan15.java

--- prak-asd/jobsheet-10/AntrianLayanan15.java
+++ prak-asd/jobsheet-10/AntrianLayanan15.java
@@ -88,4 +88,14 @@ public class AntrianLayanan15 {
   public int getJumlahAntrian() {
     return size;
   }
+
+  public void lihatAkhir() {
+    if (isEmpty()) {
+      System.out.println("Antrian kosong.");
+    } else {
+      System.out.println("Mahasiswa paling belakang: ");
+      System.out.println("NIM - NAMA - PRODI - KELAS");
+      data[rear].tampilkanData();
+    }
+  }
 }
